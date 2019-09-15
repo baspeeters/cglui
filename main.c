87581fc2d0bfacb8afa16b9bd6d6977b2e153d5b
Bas Peeters <bas@peete.rs>
2019-09-15 08:42:29 +0200

    Initial commit

---
+++ main.c
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.c
+ * Author: Bas Peeters <bas@peete.rs>
+ *
+ * Created on September 15, 2019, 8:38 AM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char** argv) {
+    return (EXIT_SUCCESS);
+}
+
