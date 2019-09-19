4522a198bed50991bf657949156eb63cc5e235c4
Bas Peeters <bas@peete.rs>
2019-09-19 14:01:27 +0200

    Initialize more than 1 rectangle Closes #2

--- main.c
+++ main.c
@@ -36,8 +36,24 @@ struct g_resources_struct {
     GLfloat fade_factor;
 };
 
-struct g_resources_struct g_resources;
+struct Rect {
+    struct {
+        GLfloat width;
+        GLfloat height;
 
+        struct {
+            GLfloat x;
+            GLfloat y;
+        } position;
+    } dimensions;
+
+    float vertex_buffer_data[8];
+    struct g_resources_struct resources;
+};
+
+static const GLushort rect_element_buffer_data[] = {0, 1, 2, 3};
+
+static GLuint default_vertex_shader, default_fragment_shader;
 
 static GLuint make_buffer(
         GLenum target,
@@ -112,22 +128,8 @@ static GLuint make_program(GLuint vertex_shader, GLuint fragment_shader) {
     return program;
 }
 
-static const GLushort g_element_buffer_data[] = {0, 1, 2, 3};
-
-struct dimensions {
-    GLfloat width;
-    GLfloat height;
-
-    struct {
-        GLfloat x;
-        GLfloat y;
-    } position;
-};
-
-struct Rect {
-    struct dimensions dimensions;
-    float vertex_buffer_data[8];
-};
+static size_t n_rects = 2;
+static struct Rect* rects;
 
 struct Rect make_rect(
         GLfloat width,
@@ -135,89 +137,98 @@ struct Rect make_rect(
         GLfloat x,
         GLfloat y
 ) {
-    struct Rect s = {width, height, x, y,
-                     x, y,
-            x + width, y,
-                     x, y - height,
-            x + width, y - height,
-    };
+    struct Rect s = {width, height, x, y, {
+            x,          y,
+            x + width,  y,
+            x,          y - height,
+            x + width,  y - height,
+    }};
+
+    s.resources.vertex_shader = default_vertex_shader;
+    s.resources.fragment_shader = default_fragment_shader;
+
+    s.resources.vertex_buffer = make_buffer(
+            GL_ARRAY_BUFFER,
+            s.vertex_buffer_data,
+            sizeof(s.vertex_buffer_data)
+    );
+
+    s.resources.element_buffer = make_buffer(
+            GL_ELEMENT_ARRAY_BUFFER,
+            rect_element_buffer_data,
+            sizeof(rect_element_buffer_data)
+    );
+
+    s.resources.program = make_program(
+            s.resources.vertex_shader,
+            s.resources.fragment_shader
+    );
+
+    s.resources.uniforms.fade_factor = glGetUniformLocation(s.resources.program, "fade_factor");
+    s.resources.attributes.position = glGetAttribLocation(s.resources.program, "position");
 
     return s;
 }
 
 static int make_resources(void) {
-    struct Rect s1 = make_rect(0.01f, 0.55f, -0.75f, 0.9f);
-
-    g_resources.vertex_buffer = make_buffer(
-            GL_ARRAY_BUFFER,
-            s1.vertex_buffer_data,
-            sizeof(s1.vertex_buffer_data)
-    );
-    g_resources.element_buffer = make_buffer(
-            GL_ELEMENT_ARRAY_BUFFER,
-            g_element_buffer_data,
-            sizeof(g_element_buffer_data)
-    );
-
-    g_resources.vertex_shader = make_shader(
+    default_vertex_shader = make_shader(
             GL_VERTEX_SHADER,
             "shaders/simple.v.glsl"
     );
-    if (g_resources.vertex_shader == 0)
+    if (default_vertex_shader == 0)
         return 0;
 
-    g_resources.fragment_shader = make_shader(
+    default_fragment_shader = make_shader(
             GL_FRAGMENT_SHADER,
             "shaders/simple.f.glsl"
     );
-    if (g_resources.fragment_shader == 0)
+    if (default_vertex_shader == 0)
         return 0;
 
-    g_resources.program = make_program(
-            g_resources.vertex_shader,
-            g_resources.fragment_shader
-    );
-    if (g_resources.program == 0)
-        return 0;
+    rects = malloc(n_rects * sizeof(struct Rect));
+    rects[0] = make_rect(0.01f, 0.55f, -0.75f, 0.9f);
+    rects[1] = make_rect(0.05f, 0.55f, 0.5f, -0.1f);
 
-    g_resources.uniforms.fade_factor = glGetUniformLocation(g_resources.program, "fade_factor");
-    g_resources.attributes.position = glGetAttribLocation(g_resources.program, "position");
+    if (rects[0].resources.fragment_shader == 0 || rects[0].resources.vertex_shader == 0)
+        return 0;
 
     return 1;
 }
 
 static void update_fade_factor(void) {
     int milliseconds = glutGet(GLUT_ELAPSED_TIME);
-    g_resources.fade_factor = sinf((float)milliseconds * 0.001f) * 0.5f + 0.5f;
+    rects[0].resources.fade_factor = sinf((float)milliseconds * 0.001f) * 0.5f + 0.5f;
     glutPostRedisplay();
 }
 
 static void render(void) {
-    glUseProgram(g_resources.program);
+    for (size_t i=0; i<n_rects; i++) {
+        glUseProgram(rects[i].resources.program);
 
-    glUniform1f(g_resources.uniforms.fade_factor, g_resources.fade_factor);
+        glUniform1f(rects[i].resources.uniforms.fade_factor, rects[i].resources.fade_factor);
 
-    glBindBuffer(GL_ARRAY_BUFFER, g_resources.vertex_buffer);
-    glVertexAttribPointer(
-            g_resources.attributes.position,
-            2,
-            GL_FLOAT,
-            GL_FALSE,
-            0,
-            (void *) 0
-    );
-    glEnableVertexAttribArray(g_resources.attributes.position);
+        glBindBuffer(GL_ARRAY_BUFFER, rects[i].resources.vertex_buffer);
+        glVertexAttribPointer(
+                rects[i].resources.attributes.position,
+                2,
+                GL_FLOAT,
+                GL_FALSE,
+                0,
+                (void *) 0
+        );
+        glEnableVertexAttribArray(rects[i].resources.attributes.position);
 
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, g_resources.element_buffer);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, rects[i].resources.element_buffer);
 
-    glDrawElements(
-            GL_TRIANGLE_STRIP,
-            sizeof(g_element_buffer_data),
-            GL_UNSIGNED_SHORT,
-            (void *) 0
-    );
+        glDrawElements(
+                GL_TRIANGLE_STRIP,
+                sizeof(rect_element_buffer_data),
+                GL_UNSIGNED_SHORT,
+                (void *) 0
+        );
 
-    glDisableVertexAttribArray(g_resources.attributes.position);
+        glDisableVertexAttribArray(rects[i].resources.attributes.position);
+    }
 
     glutSwapBuffers();
 }
